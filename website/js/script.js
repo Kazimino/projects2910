e255ef67f5db6174f9188faba493aadff6ea31e8
drwatley <atoussi@my.bcit.ca>
2016-05-10 21:43:01 -0700

    added back button to ingame screen moves transition back but still problems with large hex being small again

--- website/js/script.js
+++ website/js/script.js
@@ -6,17 +6,58 @@ $(document).ready(function() {
     }
     resizeMain();
     
+    var backbutton = false;
     var enlarged = false;
-    var hexInuse = 0;    
-    
+    var stateMem = 0;
+    var mod = 0;
+
+
     $('.module').click(function() {
+        mod = $(this).attr('id');
+        
         if(enlarged == false) {
             enlarged = true; 
+            backbutton = true;
+            
+            $('#backbutton').css({
+                    opacity: 0.0, 
+                    visibility: "visible"}).animate({
+                    opacity: 1.0});
+            
+            stateMem = {
+                top:    $(mod).css('top'),
+                bottom: $(mod).css('bottom'),
+                right:  $(mod).css('right'),
+                left:   $(mod).css('left')
+            };
+            
             $(this).siblings().hide();
-            $(this).animate({top:'5%', left:'13%'}, 500);
-            $(this).animate({height:'75%', width:'75%'}, 750);
+            $(this).animate({
+                        height:'75%', 
+                        width:'75%',   
+                        top:'0%',  
+                        left:'13%'}
+                        , 500);
+        }
+    });
+    
+    $('#backbutton').click(function() {
+        if(backbutton == true) {
+            $('.module').siblings().show();  
+            $(mod).animate({
+                stateMem, 
+                height:'10%',
+                width:'10%'}, 250);
             
+            enlarged = false;
+            backbutton = false;
             
+            $('#backbutton').animate({
+                visibility: "hidden", 
+                opacity: 0.0});
+            
+        } else {
+            return;
         }
     });
     
