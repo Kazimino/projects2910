99b58f1c133243df8b61a12fa252decf9c27737d
mtaylor8878 <mtaylor8878@gmail.com>
2016-05-17 19:22:06 +0000

    changed leaderboard times to show as MM:SS:DD

--- website/leaderboard/get_records.php
+++ website/leaderboard/get_records.php
@@ -2,7 +2,7 @@
 $db_con = mysqli_connect("my-db-identifier.cyo63eekgut2.us-west-2.rds.amazonaws.com:3306","db_user","reactor8878","reactor_db");
 
 if(mysqli_connect_errno()) {
-	echo "Error: " . mysqli_connect_errno();
+    echo "Error: " . mysqli_connect_errno();
 }
 
 $offset = array_key_exists("offset", $_GET) ? $_GET["offset"] : "0";
@@ -18,10 +18,13 @@ $result = mysqli_query($db_con, $select)
 
 $count = 1;
 while($row = mysqli_fetch_array($result)) {
+    $min = floor($row['time'] / 600);
+    $sec = floor($row['time'] % 600 / 10);
+    $dsec = $row['time'] % 10;
     echo "<li id=\"record-".$row['recordID']."\">
             <span class=\"lbRank\">".$count++."</span>
             <span class=\"lbName\">".$row['playerName']."</span>
-            <span class=\"lbTime\">".$row['time']."</span>
+            <span class=\"lbTime\">".($min == 0 ? "0" : $min).":".($sec < 10 ? "0" . $sec : $sec).":".$dsec."</span>
         </li>";
 }
 ?>
