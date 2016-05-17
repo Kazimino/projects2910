090b4840370e82111e3f2ff8990271ec911e7c12
mtaylor8878 <mtaylor8878@gmail.com>
2016-05-17 14:34:31 -0700

    added endGame overlay prototype:

--- website/js/script.js
+++ website/js/script.js
@@ -76,7 +76,6 @@ var dsec = 0;
 var totalHeat = 0;
 var activeArray = [];
 var activeMini = 0;
-var posList = ['top', 'topLeft', 'topRight', 'center', 'bottomLeft', 'bottomRight', 'bottom'];
 
 var clock;
 var timer;
@@ -170,6 +169,7 @@ function timerStart(){
 function spawnRandomGame() {
     if(activeArray.length < 7) {
         var gameLocation;
+        var posList = ['top', 'topLeft', 'topRight', 'center', 'bottomLeft', 'bottomRight', 'bottom'];
         do {
             gameLocation = posList[Math.floor((Math.random() * 7))];
         } while(activeArray[gameLocation] != null);
@@ -202,6 +202,81 @@ function spawnModule(pos) {
         $("#mini-" + activeMini).data("pos", pos);
         $("#mini-" + activeMini++ + " .gauge-fill").addClass('mini-' + pos);
     }
+}
+
+/* checks the answer to the math equation 
+mathAnswer is the answer to the question 
+pos is the position of the hex 
+$clicked is the text from the clicked button*/
+function checkMathAnswer(pos, $clicked) {
+    if($clicked == activeArray[pos].answer) {
+        endGame(pos);
+    } else {
+        //need a function to show that answer was
+        wrongAnswer();
+        //wrong and they need to keep trying 
+    }
+    
+}
+
+/* logic for the math equation game */
+function mathGame() {
+    /*controls the difficulty of the numbers and oerators */
+    var diffMultiplier = 2;
+    var gameArr = [];
+
+    /*this function grabs a random number */
+    function getRandomNumber(max) {
+        return Math.floor(Math.random() * max) + 1;
+    } 
+
+    /*this function supplies a random number for 
+    operator selection */
+    function getRandomOperator(max) {
+        return Math.floor(Math.random() * max);
+    }
+
+    /*variables for the equation and holding answers*/
+    var numOne = getRandomNumber(10 * diffMultiplier);
+    var numTwo = getRandomNumber(10 * diffMultiplier);
+    var operator = null;
+    var answer = null;
+    var equation = null;
+
+    /*check needed to stop operator increment*/
+    if(diffMultiplier < 4) {
+        operator = getRandomOperator(diffMultiplier++);
+    }
+
+    /*switch to get the answer for the RHS of the equation*/
+    switch(operator) {
+        case 0:
+            answer = numOne + numTwo;
+            operator = "+";
+            break;
+        case 1:
+            answer = numOne - numTwo;
+            operator = "-";
+            break;
+        case 2:
+            answer = numOne * numTwo;
+            operator = "*";
+            break;
+        case 3: 
+            answer = numOne / numTwo;
+            operator = "/";
+            break;
+        default:
+            break;
+    }
+
+    equation = numOne + " _ " + numTwo  + " = " 
+                   + (Math.round(answer * 100) / 100);
+    gameArr[0] = equation;
+    //$('#prob').text(equation);
+    gameArr[1] = operator;
+    
+    return gameArr;
 }
 
 function loadGame(pos) {
@@ -278,7 +353,7 @@ function loadLeaderBoard() {
     scoresLoaded = 0;
     $(".menu").hide();
     $('header, .leaderBoard').fadeIn(500);
-    ajaxGetScores(scoresLoaded);
+    ajaxGetScores();
 }
 
 function showFrame() {
@@ -288,12 +363,12 @@ function showFrame() {
     });
 }
 
-function ajaxGetScores(loaded) {
+function ajaxGetScores() {
     $.ajax({
         type: 'GET',
         url: '/leaderboard/get_records.php',
         data: {
-            offset: loaded,
+            offset: scoresLoaded,
         },
         success: function (response) {
             $('#leaderList').html($('#leaderList').html() + response);
