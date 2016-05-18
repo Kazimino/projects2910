af259d13702ad05d6338e3a07118957b2a0cf4ec
drwatley <atoussi@my.bcit.ca>
2016-05-17 20:21:12 -0700

    split up javascript for math game as requested frends

--- website/js/script.js
+++ website/js/script.js
@@ -61,7 +61,7 @@ $(document).ready(function() {
         var correct = false;
         $clicked = $(this).text().trim();
         
-        checkMathAnswer(enlarged, $clicked)
+        checkMathAnswer(enlarged, $clicked);
         
     });
     
@@ -220,82 +220,6 @@ function spawnModule(pos) {
         $("#mini-" + activeMini).data("pos", pos);
         $("#mini-" + activeMini++ + " .gauge-fill").addClass('mini-' + pos);
     }
-}
-
-/* checks the answer to the math equation 
-mathAnswer is the answer to the question 
-pos is the position of the hex 
-$clicked is the text from the clicked button*/
-function checkMathAnswer(pos, $clicked) {
-    if($clicked == activeArray[pos].answer) {
-        endGame(pos);
-        
-    } else {
-        //need a function to show that answer was
-        wrongAnswer();
-        //wrong and they need to keep trying 
-    }
-    
-}
-
-/* logic for the math equation game */
-function mathGame() {
-    /*controls the difficulty of the numbers and oerators */
-    var diffMultiplier = 2;
-    var gameArr = [];
-
-    /*this function grabs a random number */
-    function getRandomNumber(max) {
-        return Math.floor(Math.random() * max) + 1;
-    } 
-
-    /*this function supplies a random number for 
-    operator selection */
-    function getRandomOperator(max) {
-        return Math.floor(Math.random() * max);
-    }
-
-    /*variables for the equation and holding answers*/
-    var numOne = getRandomNumber(10 * diffMultiplier);
-    var numTwo = getRandomNumber(10 * diffMultiplier);
-    var operator = null;
-    var answer = null;
-    var equation = null;
-
-    /*check needed to stop operator increment*/
-    if(diffMultiplier < 4) {
-        operator = getRandomOperator(diffMultiplier++);
-    }
-
-    /*switch to get the answer for the RHS of the equation*/
-    switch(operator) {
-        case 0:
-            answer = numOne + numTwo;
-            operator = "+";
-            break;
-        case 1:
-            answer = numOne - numTwo;
-            operator = "-";
-            break;
-        case 2:
-            answer = numOne * numTwo;
-            operator = "*";
-            break;
-        case 3: 
-            answer = numOne / numTwo;
-            operator = "/";
-            break;
-        default:
-            break;
-    }
-
-    equation = numOne + " _ " + numTwo  + " = " 
-                   + (Math.round(answer * 100) / 100);
-    gameArr[0] = equation;
-    //$('#prob').text(equation);
-    gameArr[1] = operator;
-    
-    return gameArr;
 }
 
 
