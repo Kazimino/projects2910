498fe0c6a812912417f7967a40d280b4e8817cd3
Billy Wong <ocis_nicola@hotmail.com>
2016-05-19 13:23:54 -0700

    added ajax command to fetch word from db

--- website/js/anagram.js
+++ website/js/anagram.js
@@ -10,25 +10,24 @@ $(document).ready(function() {
 
         var currInput = $('#anagramInput').text();
 
-        if(currInput.length == activeArray[enlarged].answer[0].length){
+        if(currInput.length == activeArray[enlarged].answer.length){
+            if(checkWord(currInput)){
+                $('#anagramAnswer').html("<h1>" + "You Win!" + "</h1>");
+                endGame(enlarged);
 
-                if(currInput == activeArray[enlarged].answer[0]){
-                    $('#anagramAnswer').html("<h1>" + "You Win!" + "</h1>");
-                    endGame(enlarged);
-                    
-                } else {
-                    wrongAnswer();
-                    $('#anagramAnswer').html("<h1>" + "You fucked up" + "</h1>");
-                    activeArray[enlarged].input = [];
-                    activeArray[enlarged].data = [];
-                    $('#anagramInput').html("<h1></h1>");
-                    setTimeout(function(){
-                        $('.letterChoice').each(function(){
-                            $(this).fadeTo("fast", 1);
-                            $(this).css("pointer-events", "auto");
-                        });
-                    }, 600);
-                }
+            } else {
+                wrongAnswer();
+                $('#anagramAnswer').html("<h1>" + "You fucked up" + "</h1>");
+                activeArray[enlarged].input = [];
+                activeArray[enlarged].data = [];
+                $('#anagramInput').html("<h1></h1>");
+                setTimeout(function(){
+                    $('.letterChoice').each(function(){
+                        $(this).fadeTo("fast", 1);
+                        $(this).css("pointer-events", "auto");
+                    });
+                }, 600);
+            }
 
         }
 
@@ -42,13 +41,16 @@ $(document).ready(function() {
 });
 
 function checkWord(word){
-
+    if(word == activeArray[enlarged].answer){
+        return true;
+    }
+    return false;
 }
 
 function loadAnagram(){
     $('#anagramInput').html("<h1>" + activeArray[enlarged].input + "</h1>");
     $('#anagramAnswer').html("<h1>" + activeArray[enlarged].answer[0] + "</h1>");
-    var scrambled = activeArray[enlarged].answer[1].split("");
+    var scrambled = activeArray[enlarged].answer.split("");
     var pressed = activeArray[enlarged].data;
     var i = 0;
     $('.letterChoice').each(function(){
@@ -63,17 +65,22 @@ function loadAnagram(){
     })
 }
 
+function getWordFromDictionary(size, difficulty){
+
+    return $.ajax({
+        type: 'GET',
+        url: '../dictionary/get_word.php',
+        data: {length: size, rank: difficulty}
+    });
+
+    return "doggy";
+}
+
 /* Creates a new Anagram game */
 function generateAnagram(){
     var size = 4;
     var difficulty = 4;
-    var selectedWord = function(){
-        $.ajax({
-            type: 'GET',
-            url: '../dictionary/get_word.php',
-            data: {length: size, rank: difficulty}
-        })
-    };
+    var selectedWord = getWordFromDictionary(size, difficulty);
         
     var lettersArray = selectedWord.split("");
 
@@ -87,6 +94,6 @@ function generateAnagram(){
     }
 
     var scrambled = lettersArray.join("");
-    return [selectedWord, scrambled];
+    return scrambled;
 }
 
