62e47547d6ea7b70b3d1341299163ed4f43fee56
mtaylor8878 <mtaylor8878@gmail.com>
2016-05-19 08:58:43 -0700

    testing upload script

--- website/dictionary/dictionary-upload.php
+++ website/dictionary/dictionary-upload.php
@@ -6,12 +6,19 @@ $query = "SELECT * FROM Dictionary LIMIT 1;";
 $result = $db_con->query($query);
 if($result->num_rows == 0) {
     $dict = fopen("dictionary-raw.txt", "r");
+    echo "<b>Inserting Words: </b><br>"
+    $query = "INSERT INTO Dictionary (word, length) VALUES";
     while(!feof($dict)) {
         $line = fgets($dict);
         if($line.length >= 3 && $line.length <= 6) {
-            
+            $query .= " ('$line', " . $line.length . "),";
+            echo $line . "<br>";
         }
     }
+    $query = rtrim($query, ",") . ";";
+    $db_con->query($query);
     fclose($dict);
+} else {
+    echo "<b>error: Dictionary already populated</b>";
 }
 ?>
