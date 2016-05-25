a05913a8569796df4776c7f5204c9f5c1a545dae
mtaylor8878 <mtaylor8878@gmail.com>
2016-05-25 14:35:41 -0700

    initial changes to add login functionality

---
+++ website/account/menu.php
@@ -0,0 +1,20 @@
+<ul class="menu">
+    <?php if(isset($_SESSION["userid"])) {
+        echo "<a href=\"my-account\"><li>My Account</li></a>";
+        echo "<li id=\"logout\">Logout</li>";
+    } else {
+        echo "<li id=\"login\">Login</li>";
+        echo "<li id=\"loginForm\">";
+            echo "<input id=\"loginName\" type=\"text\" placeholder=\"Username\">";
+            echo "<input id=\"loginPassword\" type=\"password\" placeholder=\"Password\">";
+            echo "<input id=\"loginSubmit\" type=\"button\" value=\"Login\">";
+        echo "</li>";
+        echo "<li id=\"register\">Register</li>";
+        echo "<li id=\"registerForm\">";
+            echo "<input id=\"registerName\" type=\"text\" placeholder=\"Username\">";
+            echo "<input id=\"registerPassword\" type=\"password\" placeholder=\"Password\">";
+            echo "<input id=\"registerSubmit\" type=\"button\" value=\"Register\">";
+        echo "</li>";
+    }
+    ?>
+</ul>
