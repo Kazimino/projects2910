8cf7324d2b6a8bf50f5e7cad7717de1c7c39e2ca
mtaylor8878 <mtaylor8878@gmail.com>
2016-05-26 18:38:00 -0700

    stuff

--- website/dictionary/get_word.php
+++ website/dictionary/get_word.php
@@ -2,22 +2,6 @@
 /* this file gets a word */
 include("../leaderboard/db_connect.php");
 
-<<<<<<< HEAD
-$length = array_key_exists("length", $_GET) ? $_GET["length"] : "1";
-$rank = array_key_exists("rank", $_GET) ? $_GET["rank"] : "1";
-
-$max_rank = array(3 => 849
-                 ,4 => 1713
-                 ,5 => 2296
-                 ,6 => 2767);
-
-$rand_word = rand(($rank - 1) / 10 * $max_rank[$length], $rank / 10 * $max_rank[$length]);
-
-$select = "SELECT *
-            FROM Dictionary
-            WHERE length = $length
-                AND rank = $rand_word;" 
-=======
 $length = array_key_exists("length", $_GET) ? $_GET["length"] : "4";
 
 $query = "SELECT * FROM Dictionary WHERE length = $length";
@@ -29,19 +13,11 @@ $select = "SELECT word
             FROM Dictionary
             WHERE length = $length
                 AND rank = $rand_word;"; 
->>>>>>> 463a68be37ad10073b050be4dd8247472856d5ad
 
 $result = $db_con->query($select)
     or die("Error " . mysqli_error($db_con));
 
-<<<<<<< HEAD
-$value = mysqli_fetch_object($result);
-
-echo $value['word'];
-?>
-=======
 $value =  mysqli_fetch_object($result);
 
 echo $value->word;
 ?>
->>>>>>> 463a68be37ad10073b050be4dd8247472856d5ad
